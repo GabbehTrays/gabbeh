150e54e99df573cec117fe6c0b07d6f9e4917934
‘M_4_Maniac’ <mehrbod.crud19@gmail.com>
2025-07-26 15:52:25 +0330

    path reset for images

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
     <Router>
       <Sidebar isOpen={isMenuOpen} toggleMenu={toggleMenu} />
       <Routes>
-        <Route path="/" element={<Shop />} />
+        <Route path="/store" element={<Shop />} />
         <Route path="/about-contact" element={<AboutContact />} />
       </Routes>
     </Router>
